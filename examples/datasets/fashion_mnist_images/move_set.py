2031fd92306446dbdf69b2cd8299f866df7782d7
alexandros <alexdrymonitis@gmail.com>
2022-06-29 18:12:40 +0300

    update move_set.py

--- examples/datasets/fashion_mnist_images/move_set.py
+++ examples/datasets/fashion_mnist_images/move_set.py
@@ -3,7 +3,9 @@ import subprocess
 
 
 def main():
+    os.system("mkdir predict")
     for i in range(10):
+        os.system(f"mkdir predict/{i}")
         num = int(subprocess.check_output(f"ls ./test/{i}/ | wc -l", shell=True))
         for j in range(100):
             os.system(f"mv ./test/{i}/0{num-j-1}.png ./predict/{i}/0{num-j-1}.png")
